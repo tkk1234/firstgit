ad69fb393118d98ef5d28d98d5204f1d0b3cb41b
tkk1234 <tkk1234@163.com>
2018-05-18 16:13:49 +0800

    python3.5

--- python_3.5/week2/three_layer_menu_13.py
+++ python_3.5/week2/three_layer_menu_13.py
@@ -3,7 +3,7 @@
 # @Author: tkk1234
 # @Date:   2018-05-17 11:31:40
 # @Last Modified by:   tkk1234
-# @Last Modified time: 2018-05-17 14:19:08
+# @Last Modified time: 2018-05-18 11:29:39
 menu = {
 	"北京":{
 		"昌平":{
@@ -29,3 +29,16 @@ while  True:
 	for x in menu:
 		print(x)
 
+	choice = input("选择进入>>:")
+	if choice in menu:
+		while True:
+			for x in menu[choice]:
+				print("\t",x)
+			choice2 = input("选择进入>>:")
+			if choice2 in menu[choice]:
+				while True:
+					pass
+				pass
+
+				#再测试！！！
+				##test again
---
+++ python_3.5/week3/set_01.py
@@ -0,0 +1,6 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author: tkk1234
+# @Date:   2018-05-18 16:11:04
+# @Last Modified by:   tkk1234
+# @Last Modified time: 2018-05-18 16:11:04
